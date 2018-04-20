fffef44268eea0ad41fc9fbc56680c28134e4155
Scyntre2017 <cjones999983@gmail.com>
2018-04-20 02:24:09 +0000

    environment update

--- lib/coding_resources.rb
+++ lib/coding_resources.rb
@@ -1,9 +1,6 @@
 require "pry"
 require "nokogiri"
 require "open-uri"
+require "require_all"
 
-require_relative "../lib/coding_resources/version.rb"
-require_relative "../lib/coding_resources/cli.rb"
-require_relative "../lib/coding_resources/scraper.rb"
-require_relative "../lib/coding_resources/book_list.rb"
-require_relative "../lib/coding_resources/book_details.rb"
+require_all "lib/coding_resources"
