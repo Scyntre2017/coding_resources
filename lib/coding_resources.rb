d22a526214a7cfe438897dc3e821ee4254664e42
Christopher Jones <cjones999983@gmail.com>
2018-04-20 02:13:20 +0000

    Finished initial environment setup

--- lib/coding_resources.rb
+++ lib/coding_resources.rb
@@ -1,5 +1,9 @@
-require "coding_resources/version"
+require "pry"
+require "nokogiri"
+require "open-uri"
 
-module CodingResources
-  # Your code goes here...
-end
+require_relative "../lib/coding_resources/version.rb"
+require_relative "../lib/coding_resources/cli.rb"
+require_relative "../lib/coding_resources/scraper.rb"
+require_relative "../lib/coding_resources/book_list.rb"
+require_relative "../lib/coding_resources/book_details.rb"
