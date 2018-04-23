85aa9b9d37b24feb8e3d794e27a64fdd24264bef
Christopher Jones <cjones999983@gmail.com>
2018-04-23 02:24:28 +0000

    added Book functions 'self.all', 'self.list', and 'initialize' to Book class

--- lib/coding_resources/books.rb
+++ lib/coding_resources/books.rb
@@ -1,7 +1,23 @@
 class CodingResources::Books
 
+  attr_accessor :name, :url
+
+  @@all = []
+
+  def initialize(book_hash)
+    @name = book_hash.values_at(:name).join
+    @url = book_hash.values_at(:url).join
+    @@all << self unless CodingResources::Books.find_by_name(self.name)
+  end
+
+  def self.all
+    @@all
+  end
+
   def self.list
-    puts "hello"
+    self.all.each.with_index(1) do |book, i|
+      puts "#{i}. #{book.name}"
+    end
   end
 
 end
