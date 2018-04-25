1df72fff2e06066233fa2726504180a4c3561823
Christopher Jones <cjones999983@gmail.com>
2018-04-25 03:42:48 +0000

    finished CLI functionality

--- lib/coding_resources/cli.rb
+++ lib/coding_resources/cli.rb
@@ -6,9 +6,10 @@ class CodingResources::CLI
 
   def menu
     puts "Welcome to Coding Resources"
-    puts "Please type 'list' for a list of books or type 'search' to search for a book or type 'exit' to exit."
+    puts "Please type 'list' for a list of all available books or type 'search' to search for a book or type 'exit' to exit."
     input = gets.strip.downcase
     if input == "list"
+      CodingResources::Books.all_books
       list
     elsif input == "search"
       puts "Please enter the name of the book or part of the name to search for."
@@ -16,18 +17,21 @@ class CodingResources::CLI
       search(i)
     elsif input == "exit"
       puts "Have a nice day!"
+      exit
     end
   end
 
   def selection
-    puts "Please type the number of the book for more information or type next for the next 25 books."
+    puts "Please type the number of the book for more information, type 'next' for the next 25 books, or type 'search' to search for a book."
     input = gets.strip
     if input.to_i > 0
       details(input.to_i - 1)
     elsif input.downcase == "next"
-      next_page
-    elsif input.downcase == "menu"
-      menu
+      CodingResources::Books.next_page
+      list
+    elsif input.downcase == "search"
+      i = gets.strip.downcase
+      search(i)
     elsif input.downcase == "exit"
       exit
     else
@@ -38,25 +42,23 @@ class CodingResources::CLI
 
   def list
     puts CodingResources::Books.list
-    # lists books in groups of 25 books each by grabbing data from the book_list class
-    # type 'next' to get to the next group
     selection
-    input = gets.strip.to_i
-    details(input.to_i - 1)
   end
 
   def search(name)
-    # searches for books from the book_list class
+    CodingResources::Books.search(name)
+    list
   end
 
   def details(number)
     puts "book details #{number}"
-    puts "Please type 'back' or 'list' to go back to the list; type 'menu' to go back to the main menu; or type 'exit' to exit."
+    puts "Please type 'back' or 'list' to go back to the list; type 'search' to search for a new book; or type 'exit' to exit."
     input = gets.strip.downcase
     if input == "back" || input == "list"
       list
-    elsif input == "menu"
-      menu
+    elsif input == "search"
+      i = gets.strip.downcase
+      seach(i)
     elsif input == "exit"
       exit
     end
