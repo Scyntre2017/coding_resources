b06a19ef88120da027f708daaf0f8f467c80b904
Christopher Jones <cjones999983@gmail.com>
2018-05-05 02:43:22 +0000

    added functionality to scrape_all_books method

--- lib/coding_resources/scraper.rb
+++ lib/coding_resources/scraper.rb
@@ -4,7 +4,13 @@ class CodingResources::Scraper
     page = 0
     books = []
     while page < 82
-      doc = Nokogiri::HTML(open("http://www.freetechbooks.com/topics?page=#{page++}"))
+      page += 1
+      doc = Nokogiri::HTML(open("http://www.freetechbooks.com/topics?page=#{page}"))
+      doc.css("p.media-heading").each do |book|
+        desc_url = book.css("a").attribute("href").value
+        name = book.text
+        books << {name: name, desc_url: desc_url}
+      end
       binding.pry
     end
 
