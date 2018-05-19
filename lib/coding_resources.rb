109cb13dda06de5f520bbc5df19d2e64f1b82341
Christopher Jones <cjones999983@gmail.com>
2018-05-19 22:22:43 +0000

    updated coding_resources.rb to fix a uninitialized constant error

--- lib/coding_resources.rb
+++ lib/coding_resources.rb
@@ -2,7 +2,7 @@ require "coding_resources"
 require "nokogiri"
 require "open-uri"
 
+require_relative "coding_resources/version"
 require_relative "coding_resources/cli"
 require_relative "coding_resources/books"
 require_relative "coding_resources/scraper"
-require_relative "coding_resources/version"
